27c566e96db284c50360079bd53eb6c97013238b
roqurdl <uyhnri@gmail.com>
2022-09-19 15:43:18 +0900

    littleFun

--- src/views/app.js
+++ src/views/app.js
@@ -1,4 +1,41 @@
 const canvas = document.querySelector(`canvas`);
+const ctx = canvas.getContext("2d");
 
-canvas.height = 500;
-canvas.width = 500;
+const CANVAS_HEIGHT = 500;
+const CANVAS_WIDTH = 500;
+
+canvas.height = CANVAS_HEIGHT;
+canvas.width = CANVAS_WIDTH;
+
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+  "#18dcff",
+  "#7d5fff",
+];
+
+let xCoord = 0;
+let yCoord = 0;
+
+function onClick(e) {
+  ctx.beginPath();
+  xCoord = e.offsetX;
+  yCoord = e.offsetY;
+}
+
+function canvasOnMove(e) {
+  ctx.beginPath();
+  ctx.moveTo(xCoord, yCoord);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+
+canvas.addEventListener(`click`, onClick);
+canvas.addEventListener(`mousemove`, canvasOnMove);
